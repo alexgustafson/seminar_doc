82344c1a5bc8cad3f0a13ee1deaad57aba846df2
Alex Gustafson <alex.gustafson@netzbarkeit.ch>
2015-06-29 12:15:12 +0200

    initial commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,28 @@
 $(function () {
-    var content_height = 752;	// the height of the content, discluding the header/footer
+    var content_height = 772;	// the height of the content, discluding the header/footer
     var page = 1;				// the beginning page number to show in the footer
+
+    function buildCoverPage() {
+        if ($('#coverPage').contents().length > 0) {
+            $page = $(".cover_template:first").clone().addClass("page").css("display", "block");
+            $("body").append($page);
+
+            // here is the columnizer magic
+            $('#coverPage').columnize({
+                columns: 1,
+                target: ".page:last .content",
+                overflow: {
+                    height: content_height,
+                    id: "#coverPage",
+                    doneFunc: function () {
+                        console.log("done with cover");
+                        buildCoverPage();
+                    }
+                }
+            });
+        }
+    }
+
     function buildNewsletter() {
         if ($('#newsletterContent').contents().length > 0) {
             // when we need to add a new page, use a jq object for a template
@@ -23,8 +45,9 @@ $(function () {
                     }
                 }
             });
+
         }
     }
-
-    setTimeout(buildNewsletter, 3000);
+    setTimeout(buildCoverPage, 100);
+    setTimeout(buildNewsletter, 300);
 });
