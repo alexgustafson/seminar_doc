69729e43da448b4d31ab1c3a818ef2e73250cd9a
Alex Gustafson <alex.gustafson@netzbarkeit.ch>
2015-06-30 17:35:01 +0200

    initial commit

---
+++ app/js/handout.js
@@ -0,0 +1,53 @@
+$(function () {
+    var content_height = 850;	// the height of the content, discluding the header/footer
+    var page = 1;				// the beginning page number to show in the footer
+
+    function buildCoverPage() {
+        if ($('#coverPage').contents().length > 0) {
+            $page = $(".cover_template:first").clone().addClass("page").css("display", "block");
+            $("body").append($page);
+
+            // here is the columnizer magic
+            $('#coverPage').columnize({
+                columns: 1,
+                target: ".page:last .content",
+                overflow: {
+                    height: content_height,
+                    id: "#coverPage",
+                    doneFunc: function () {
+                        console.log("done with cover");
+                        buildCoverPage();
+                    }
+                }
+            });
+        }
+    }
+
+    function buildNewsletter() {
+        if ($('#handout').contents().length > 0) {
+            // when we need to add a new page, use a jq object for a template
+            // or use a long HTML string, whatever your preference
+            $page = $(".page_template:first").clone().addClass("page").css("display", "block");
+            // fun stuff, like adding page numbers to the footer
+            $page.find(".footer span").append(page);
+            $("body").append($page);
+            //page++;
+            // here is the columnizer magic
+            $('#handout').columnize({
+                columns: 2,
+                target: ".page:last .content",
+                overflow: {
+                    height: content_height,
+                    id: "#handout",
+                    doneFunc: function () {
+                        console.log("done with page");
+                        buildNewsletter();
+                    }
+                }
+            });
+
+        }
+    }
+    setTimeout(buildCoverPage, 100);
+    setTimeout(buildNewsletter, 300);
+});
